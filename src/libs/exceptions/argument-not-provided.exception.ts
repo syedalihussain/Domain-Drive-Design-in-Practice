f6a929317abff5291b0fbc1e9b3f7aaf07a8a6a6
Syed Ali Hussain <ali@veridooh.com>
2022-03-19 14:34:29 +1100

    Create ArgumentNotProvidedException class

---
+++ src/libs/exceptions/argument-not-provided.exception.ts
@@ -0,0 +1,6 @@
+import { ExceptionBase } from './exception.base';
+import { ExceptionCodes } from './exception.codes';
+
+export class ArgumentNotProvidedException extends ExceptionBase {
+  readonly code = ExceptionCodes.argumentNotProvided;
+}
