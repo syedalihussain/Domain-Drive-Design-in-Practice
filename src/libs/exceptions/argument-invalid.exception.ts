258995730b5f1668c07a9e7561717a765e681d7f
Syed Ali Hussain <ali@veridooh.com>
2022-03-23 23:10:40 +1100

    Create an ArgumentInvalidException class

---
+++ src/libs/exceptions/argument-invalid.exception.ts
@@ -0,0 +1,6 @@
+import { ExceptionBase } from "./exception.base";
+import { ExceptionCodes } from "./exception.codes";
+
+export class ArgumentInvalidException extends ExceptionBase {
+  readonly code = ExceptionCodes.argumentInvalid;
+}
