cfe18502b92ef139ccda839d7bee1e3b45c6bb4b
Syed Ali Hussain <ali@veridooh.com>
2022-03-26 22:54:15 +1100

    Convert props to object

---
+++ src/libs/ddd/domain/utils/convert-props-to-object.util.ts
@@ -0,0 +1,35 @@
+import { Entity } from '../base-classes/entity.base';
+import { ValueObject } from '../base-classes/value-object.base';
+
+function isEntity(obj: unknown): obj is Entity<unknown> {
+  return (
+    Object.prototype.hasOwnProperty.call(obj, 'toObject') &&
+    Object.prototype.hasOwnProperty.call(obj, 'id') &&
+    ValueObject.isValueObject((obj as Entity<unknown>).id)
+  )
+}
+
+function convertToPlainObject(item: any): any {
+  if (ValueObject.isValueObject(item)) {
+    return item.unpack();
+  }
+  if (isEntity(item)) {
+    return item.toObject();
+  }
+  return item;
+}
+
+export function convertPropsToObject(props: any): any {
+  const propsCopy = { ...props };
+
+  for (const prop in propsCopy) {
+    if (Array.isArray(propsCopy[prop])) {
+      propsCopy[prop] = (propsCopy[prop] as Array<unknown>).map(item => {
+        return convertToPlainObject(item);
+      });
+    }
+    propsCopy[prop] = convertToPlainObject(propsCopy[prop]);
+  }
+
+  return propsCopy;
+}
